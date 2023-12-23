00bb9e17a5921a234c58fd1bb400d8d30f922a82
Aditia Kurniawan <aditiakurniawanx@gmail.com>
2023-12-23 16:16:29 +0700

    responsive tailwind

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,9 +26,9 @@ export default function Dashboard() {
       });
   };
   return (
-    <main className="w-screen min-h-screen flex flex-col bg-gradient-to-br from-orange-800 to-orange-500 mx-auto p-10">
-      <div className="w-1/4 mx-auto bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6 items-center">
-        <h1 className="text-4xl text-blue-500 text-center font-bold">
+    <main className="w-screen min-h-screen flex flex-col bg-gradient-to-br from-orange-800 to-orange-500 mx-auto p-10 max-w-[640px] sm:max-w-full md:max-w-full">
+      <div className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6 items-center">
+        <h1 className="text-4xl text-orange-800 text-center font-bold">
           Welcome
         </h1>
         <img
@@ -36,8 +36,8 @@ export default function Dashboard() {
           alt=""
           className="w-20 h-20 rounded-full mx-auto object-cover"
         />
-        <p className="text-2xl text-blue-500 ">{user?.displayName}</p>
-        <p className="text-2xl text-blue-500 ">{user?.email}</p>
+        <h2 className=" text-orange-500 ">{user?.displayName}</h2>
+        <h3 className=" text-orange-500 ">{user?.email}</h3>
         <button
           className="w-3/4 h-10 bg-black text-white rounded-xl mt-10"
           onClick={handleLogout}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,14 +23,14 @@ export default function Login() {
       });
   };
   return (
-    <main className="w-screen min-h-screen flex flex-col bg-gradient-to-br from-blue-800 to-blue-500 mx-auto p-10">
+    <main className="w-screen min-h-screen flex flex-col bg-gradient-to-br from-blue-800 to-blue-500 mx-auto p-10 max-w-[640px] sm:max-w-full md:max-w-full">
       <form
         action=""
-        className="w-1/4 mx-auto bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6"
+        className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/4 mx-auto bg-white flex flex-col gap-4 shadow-lg rounded-lg mt-8 p-6"
         autoComplete="off"
         onSubmit={handleLogin}
       >
-        <h1 className="text-4xl text-blue-500 text-center font-bold">Login</h1>
+        <h1 className="text-4xl text-blue-800 text-center font-bold">Login</h1>
         <div className="flex flex-col gap-2">
           <label htmlFor="email" autoComplete="off">
             Email
